a0f07a5f61d2974c9d66b9a3fad3f23398e60ffd
Rodrigo <rodrigoarayamunoz.infor@gmail.com>
2026-03-19 16:06:24 -0300

    conexion y configuracion completa con docker y postgresql

--- src/backend/compatibilties/alembic/env.py
+++ src/backend/compatibilties/alembic/env.py
@@ -1,33 +1,27 @@
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
-
 from alembic import context
+from sqlalchemy import engine_from_config, pool
 
-from models.base import Base;
-from models.ml_token import MLToken;
 from config import settings
+from models.base import Base
+from models.ml_token import MLToken
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
 config = context.config
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
 target_metadata = Base.metadata
 
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
+
+def get_sync_database_url() -> str:
+    db_url = settings.database_url
+
+    if db_url.startswith("postgresql+asyncpg://"):
+        return db_url.replace("postgresql+asyncpg://", "postgresql+psycopg://", 1)
+
+    return db_url
 
 
 def run_migrations_offline() -> None:
@@ -52,24 +46,18 @@ def run_migrations_online() -> None:
         configuration,
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
-        future = True,
+        future=True,
     )
 
     with connectable.connect() as connection:
         context.configure(
-            connection=connection, target_metadata=target_metadata, compare_type=True
+            connection=connection,
+            target_metadata=target_metadata,
+            compare_type=True,
         )
 
         with context.begin_transaction():
             context.run_migrations()
-
-def get_sync_database_url() -> str:
-    db_url = settings.database_url
-
-    if db_url.startswith("postgresql+asyncpg://"):
-        return db_url.replace("postgresql+asyncpg://", "postgresql+psycopg://", 1)
-
-    return db_url
 
 
 if context.is_offline_mode():
